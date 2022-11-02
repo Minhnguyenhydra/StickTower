b73d0cbd05da420cc7dc18b25e664966df403deb
BT <nthanh191@gmail.com>
2022-11-02 19:20:06 +0700

    ~ Disble tut when finish

--- Assets/__Game__Play__+/Scripts/Tut/0_GamePlay/Tut_0_Game_Play.cs
+++ Assets/__Game__Play__+/Scripts/Tut/0_GamePlay/Tut_0_Game_Play.cs
@@ -8,6 +8,9 @@ public class Tut_0_Game_Play : Singleton<Tut_0_Game_Play>
     public Enum_Anim_0 enum_anim_0;
     public int index = 0;
     public bool isfff;
+    public bool allowTut2 = true;
+
+
     private void Start()
     {
         anim = GetComponent<Animator>();
@@ -19,19 +22,10 @@ public class Tut_0_Game_Play : Singleton<Tut_0_Game_Play>
     public void SetCheck_State()
     {
         int ii = Drag_Drop_Manager.Instance.index_Step_Tut_0_use;
+        Debug.Log(ii);
+        if (ii == -1 || ii == 2)
+            SetChange_State(Enum_Anim_0.anim_03);
 
-        if (ii == -1)
-        {
-            SetChange_State(Enum_Anim_0.anim_03);
-        }
-        if (ii == 1)
-        {
-            SetChange_State(Enum_Anim_0.anim_02);
-        }
-        if (ii == 2)
-        {
-            SetChange_State(Enum_Anim_0.anim_03);
-        }
     }
     public void SetChange_State(Enum_Anim_0 new_State)
     {
@@ -54,23 +48,30 @@ public class Tut_0_Game_Play : Singleton<Tut_0_Game_Play>
     }
     public void Set_Tut_0_2()
     {
-        anim.ResetTrigger(Constant.Trigger_Tut_0_1);
+        anim.ResetTrigger(Constant.Trigger_Tut_0_3);
         anim.SetTrigger(Constant.Trigger_Tut_0_2);
-        if (index == 3)
-        {
-            Destroy(this.gameObject);
-        }
+        enum_anim_0 = Enum_Anim_0.anim_02;
         index++;
     }
     public void Set_Tut_0_1()
     {
+        if (index > 0)
+        {
+            Set_Tut_0_3_Off();
+            return;
+        }
+
         anim.SetTrigger(Constant.Trigger_Tut_0_1);
     }
-    
+
     public void Set_Tut_0_3_Off()
     {
         anim.ResetTrigger(Constant.Trigger_Tut_0_1);
         anim.SetTrigger(Constant.Trigger_Tut_0_3);
+        if (index == 2)
+        {
+            gameObject.SetActive(false);
+        }
         index++;
     }
 
