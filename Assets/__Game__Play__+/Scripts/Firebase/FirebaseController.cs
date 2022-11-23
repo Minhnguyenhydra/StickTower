7812397a37187aa38df22ad9481d550d73e86265
BT <nthanh191@gmail.com>
2022-11-23 22:55:21 +0700

    ~ Update networks + Add Firebase Analytics and Init it

---
+++ Assets/__Game__Play__+/Scripts/Firebase/FirebaseController.cs
@@ -0,0 +1,63 @@
+using Firebase;
+using Firebase.Analytics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FirebaseController : MonoBehaviour
+{
+    private static FirebaseController Instance;
+
+    private FirebaseApp app;
+    private int sceneIndex;
+    private string sceneName;
+
+
+    private void Start()
+    {
+        if (Instance != null)
+            return;
+
+        Instance = this;
+        Init();
+        DontDestroyOnLoad(this);
+    }
+
+    private void Init()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                app = FirebaseApp.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+            }
+            else
+            {
+                Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+
+        var activeScene = SceneManager.GetActiveScene();
+        sceneIndex = activeScene.buildIndex;
+        sceneName = activeScene.name;
+
+        FirebaseAnalytics.LogEvent(FirebaseAnalytics.EventLevelStart,
+            new Parameter(FirebaseAnalytics.ParameterLevel, sceneIndex),
+            new Parameter(FirebaseAnalytics.ParameterLevelName, sceneName));
+    }
+
+    private void OnDisable()
+    {
+        FirebaseAnalytics.LogEvent(FirebaseAnalytics.EventLevelStart,
+                                   new Parameter(FirebaseAnalytics.ParameterLevel, sceneIndex),
+                                   new Parameter(FirebaseAnalytics.ParameterLevelName, sceneName));
+    }
+}
